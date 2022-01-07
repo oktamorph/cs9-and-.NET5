108d35642eabc501f95e41bb32b3e766bf2637e2
oktamorph <oktamorph@gmail.com>
2022-01-07 22:15:23 +0100

    update the 'Number' project

--- Code/Chapter02/Numbers/Program.cs
+++ Code/Chapter02/Numbers/Program.cs
@@ -28,6 +28,10 @@ namespace Numbers
             // both statements output true
             Console.WriteLine($"{decimalNotation == binaryNotation}");
             Console.WriteLine($"{decimalNotation == hexadecimalNotation}");
+
+            Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range {int.MinValue:N0} to {int.MaxValue:N0}.");
+            Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range {double.MinValue:N0} to {double.MaxValue:N0}.");
+            Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}.");
         }
     }
 }
