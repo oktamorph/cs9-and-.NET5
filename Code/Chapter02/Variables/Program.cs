b3c32482020b50fb2e20baaa5366ce331882037a
oktamorph <oktamorph@gmail.com>
2022-02-20 18:37:08 +0100

    add the "Variables" project

---
+++ Code/Chapter02/Variables/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+object height = 1.88;
+object name = "Amir";
+Console.WriteLine($"{name} is {height} metres tall.");
+
+// int length1 = name.Length;
+int length2 = ((string)name).Length;
+Console.WriteLine($"{name} has {length2} characters.");
