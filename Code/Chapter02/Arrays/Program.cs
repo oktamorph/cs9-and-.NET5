00c52bb6ec45ac85a1de8f092d0de01825614709
oktamorph <oktamorph@gmail.com>
2022-02-21 15:34:41 +0100

    add the "Arrays" project

---
+++ Code/Chapter02/Arrays/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] names = new string[4];
+names[0] = "Kate";
+names[1] = "Jack";
+names[2] = "Rebecca";
+names[3] = "Tom";
+
+for(int i = 0; i < names.Length; i++)
+{
+    Console.WriteLine(names[i]);
+}
