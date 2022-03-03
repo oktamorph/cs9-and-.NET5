f8aaa2e11ee08797d9bd2041c0218674a59e1810
oktamorph <oktamorph@gmail.com>
2022-03-03 16:46:56 +0100

    add the 'Operators' project

---
+++ Code/Chapter03/Operators/Program.cs
@@ -0,0 +1,20 @@
+﻿int a = 3;
+int b = a++;
+Console.WriteLine($"a is {a}, b is {b}");
+
+int c = 3;
+int d = ++c;
+Console.WriteLine($"c is {c}, d is {d}");
+
+int e = 11;
+int f = 3;
+Console.WriteLine($"e is {e}, f is {f}");
+Console.WriteLine($"e + f = {e + f}");
+Console.WriteLine($"e - f = {e - f}");
+Console.WriteLine($"e * f = {e * f}");
+Console.WriteLine($"e / f = {e / f}");
+Console.WriteLine($"e % f = {e % f}");
+
+double g = 11.0;
+Console.WriteLine($"g is {g:N1}, f is {f}");
+Console.WriteLine($"g / f = {g / f}");
