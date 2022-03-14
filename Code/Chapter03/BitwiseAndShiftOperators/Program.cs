b9e9c4d1f78a78b62936b8fa2ad3577b73cab571
Oleksandr-Bolilyi <oleksandr.bolilyi@alten.se>
2022-03-14 17:12:22 +0100

    add the "BooleanOperators" and "BitwiseAndShiftOperators" projects

---
+++ Code/Chapter03/BitwiseAndShiftOperators/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 10; // 0000 1010
+int b = 6;  // 0000 0110
+
+Console.WriteLine($"a = {a}");
+Console.WriteLine($"b = {b}");
+
+Console.WriteLine($"a & b = {a & b}"); // 2-bit column only
+Console.WriteLine($"a | b = {a | b}"); // 8, 4, and 2-bit columns
+Console.WriteLine($"a ^ b = {a ^ b}"); // 8 and 4-bit columns
+
+Console.WriteLine($"a << 3 = {a << 3}"); // 0101 0000 left-shift by a three bit columns
+Console.WriteLine($"a * 8 = {a * 8}"); // multiply a by 8
+Console.WriteLine($"b >> 1 = {b >> 1}"); // right-shift b by one bit column
---
+++ Code/Chapter03/BooleanOperators/Program.cs
@@ -0,0 +1,15 @@
+﻿bool a = true;
+bool b = false;
+
+Console.WriteLine($"AND |a   |b");
+Console.WriteLine($"a | {a & a,-5} | {a & b,-5}");
+Console.WriteLine($"a | {b & a,-5} | {b & b,-5}");
+Console.WriteLine();
+Console.WriteLine($"OR |a   |b");
+Console.WriteLine($"a | {a | a,-5} | {a | b,-5}");
+Console.WriteLine($"a | {b | a,-5} | {b | b,-5}");
+Console.WriteLine();
+Console.WriteLine($"XOR |a   |b");
+Console.WriteLine($"a | {a ^ a,-5} | {a ^ b,-5}");
+Console.WriteLine($"a | {b ^ a,-5} | {b ^ b,-5}");
+Console.WriteLine();
