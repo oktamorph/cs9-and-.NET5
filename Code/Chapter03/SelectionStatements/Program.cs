35229ccfc764de240a508004f306d9a8f9853c04
oktamorph <oktamorph@gmail.com>
2022-03-15 21:50:24 +0100

    update the "Program.cs" file in the "SelectionStatements" project

--- Code/Chapter03/SelectionStatements/Program.cs
+++ Code/Chapter03/SelectionStatements/Program.cs
@@ -1,4 +1,6 @@
-﻿if (args.Length == 0)
+﻿using System.IO;
+
+if (args.Length == 0)
     Console.WriteLine("There are no arguments.");
 else
     Console.WriteLine("There is at least one argument.");
@@ -38,3 +40,63 @@ switch (number)
         Console.WriteLine("Default");
         break;
 }
+
+string path = @"C:\Users\obolilyi\source\repos\cs9-and-.NET5\Code\Chapter03";
+
+Console.Write("Press R for readonly or W for write: ");
+ConsoleKeyInfo key = Console.ReadKey();
+Console.WriteLine();
+
+Stream s = null;
+
+if (key.Key == ConsoleKey.R)
+{
+    s = File.Open(
+        Path.Combine(path, "file.txt"),
+        FileMode.OpenOrCreate,
+        FileAccess.Read);
+}
+else
+{
+    s = File.Open(
+        Path.Combine(path, "file.txt"),
+        FileMode.OpenOrCreate,
+        FileAccess.Write);
+}
+
+string message = string.Empty;
+
+switch (s)
+{
+    case FileStream writeableFile when s.CanWrite:
+        message = "The stream is a file that I can write to.";
+        break;
+    case FileStream readOnlyFile:
+        message = "The stream is a memory address";
+        break;
+    case MemoryStream:
+        message = "The stream is some other type.";
+        break;
+    default:
+        message = "The stream is some other type";
+        break;
+    case null:
+        message = "The stream is null";
+        break;
+}
+Console.WriteLine(message);
+
+message = s switch
+{
+    FileStream writeableFile when s.CanWrite
+        => "The stream is a file that I can write to.",
+    FileStream readOnlyFile
+        => "The stream is a read-only file.",
+    MemoryStream ms
+        => "The stream is a memory address.",
+    null
+        => "The stream is null.",
+    _
+        => "The stream is some other type."
+};
+Console.WriteLine(message);
