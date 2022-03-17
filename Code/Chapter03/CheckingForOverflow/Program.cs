ff824e4fb78d3057c350ed94a9a94c193243773d
oktamorph <oktamorph@gmail.com>
2022-03-17 20:39:40 +0100

    add the "CheckingForOverflow" project

---
+++ Code/Chapter03/CheckingForOverflow/Program.cs
@@ -0,0 +1,27 @@
+﻿try
+{
+    checked
+    {
+        int x = int.MaxValue - 1;
+        Console.WriteLine($"Initial value: {x}");
+        x++;
+        Console.WriteLine($"After incrementing: {x}");
+        x++;
+        Console.WriteLine($"After incrementing: {x}");
+        x++;
+        Console.WriteLine($"After incrementing: {x}");
+    }
+    unchecked
+    {
+        int y = int.MaxValue + 1;
+        Console.WriteLine($"Initial valie: {y}");
+        y--;
+        Console.WriteLine($"After decrementing: {y}");
+        y--;
+        Console.WriteLine($"After decrementing: {y}");
+    }
+}
+catch (OverflowException)
+{
+    Console.WriteLine("The code overflowed but I caught the exception.");
+}
