05e1969bd165d5dd88532c7d80f03344e21a50f7
oktamorph <oktamorph@gmail.com>
2022-03-17 13:35:07 +0100

    add the "IterationStatements" project

---
+++ Code/Chapter03/IterationStatements/Program.cs
@@ -0,0 +1,29 @@
+﻿int x = 0;
+
+while (x < 10)
+{
+    Console.WriteLine(x);
+    x++;
+}
+
+string password = string.Empty;
+
+do
+{
+    Console.Write("Enter your password: ");
+    password = Console.ReadLine();
+}
+while (password != "Pa$$w0rd");
+
+Console.WriteLine("Correct!");
+
+for (int y = 1; y <= 10; y++)
+{
+    Console.WriteLine(y);
+}
+
+string[] names = { "Adam", "Barry", "Charlie" };
+foreach(string name in names)
+{
+    Console.WriteLine($"{name} has {name.Length} characters.");
+}
