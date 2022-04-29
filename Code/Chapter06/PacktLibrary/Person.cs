9efad0988d9cc3175844a3a3c813f5f62988615d
oktamorph <oktamorph@gmail.com>
2022-04-29 17:00:53 +0200

    add the "Chapter06" project

---
+++ Code/Chapter06/PacktLibrary/Person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PacktLibrary
+{
+    public class Person
+    {
+        // fields
+        public string Name;
+        public DateTime DateOfBirth;
+        public List<Person> Children = new List<Person>();
+        public event EventHandler Shout;
+        public int AngerLevel;
+        // methods
+        public void WriteToConsole()
+        {
+            Console.WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+        }
+        public static Person Procreate(Person p1, Person p2)
+        {
+            var baby = new Person
+            {
+                Name = $"Baby of {p1.Name} and {p2.Name}"
+            };
+
+            p1.Children.Add(baby);
+            p2.Children.Add(baby);
+
+            return baby;
+        }
+        public Person ProcreateWith(Person partner)
+        {
+            return Procreate(this, partner);
+        }
+        public static Person operator *(Person p1, Person p2)
+        {
+            return Person.Procreate(p1, p2);
+        }
+        /// <summary>
+        /// Method with a local function.
+        /// </summary>
+        public static int Factorial(int number)
+        {
+            if(number < 0)
+            {
+                throw new ArgumentException($"{nameof(number)} cannot be less than zero.");
+            }
+            return LocalFactorial(number);
+            
+            int LocalFactorial(int localnumber) // local function
+            {
+                if (localnumber < 1) return 1;
+                return localnumber * LocalFactorial(localnumber - 1);
+            }
+        }/// <summary>
+        /// Method.
+        /// </summary>
+        public void Poke()
+        {
+            AngerLevel++;
+            if(AngerLevel >= 3)
+            {
+                // if something is listening...
+                Shout?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+    }
+}
---
+++ Code/Chapter06/PeopleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using PacktLibrary;
+
+namespace PeopleApp
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var harry = new Person { Name = "Harry" };
+            var mary = new Person { Name = "Mary" };
+            var jill = new Person { Name = "Jill" };
+
+            var baby1 = mary.ProcreateWith(harry);
+            baby1.Name = "Gary";
+
+            var baby2 = Person.Procreate(harry, jill);
+
+            // call an operator
+            var baby3 = harry * mary;
+
+            Console.WriteLine($"{harry.Name} has {harry.Children.Count} children.");
+            Console.WriteLine($"{mary.Name} has {mary.Children.Count} children.");
+            Console.WriteLine($"{jill.Name} has {jill.Children.Count} children.");
+            Console.WriteLine(format: "{0}'s first child is named \"{1}\".",
+                arg0: harry.Name,
+                arg1: harry.Children[0].Name);
+
+            Console.WriteLine($"5! is {Person.Factorial(5)}");
+
+            harry.Shout += Harry_Shout;
+            harry.Poke();
+            harry.Poke();
+            harry.Poke();
+            harry.Poke();
+        }
+        private static void Harry_Shout(object sender, EventArgs e)
+        {
+            Person p = (Person)sender;
+            Console.WriteLine($"{p.Name} is this angry: {p.AngerLevel}");
+        }
+    }
+}
